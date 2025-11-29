7ff011e2658b4af8015c11e6e5525752406082c0
Khalid Bin Walid <kbwalid26@gmail.com>
2025-11-29 19:08:04 +0900

    Use gruvbox dark hard theme

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,6 +4,7 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            vim.g.gruvbox_contrast_dark = 'hard'
             vim.cmd.colorscheme "gruvbox"
         end,
     },
